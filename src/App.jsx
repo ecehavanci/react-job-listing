4cf4533fb45e71f89e57bf8f122d5fe3fa7860b2
Ece Havanci <ecehavanci@gmail.com>
2024-07-30 16:53:54 +0300

    add job page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFound from './pages/NotFound';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJob from './pages/AddJob';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/add-job' element={<AddJob />} />
+    <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} /> 
     <Route path='*' element={<NotFound />} />
 
   </Route>
